060a03f74a7a319db81d7359499153b01574e0a6
Jack Chen <a0930522803@gmail.com>
2025-04-09 17:14:46 +0800

    Update

--- crawl.py
+++ crawl.py
@@ -1,36 +1,54 @@
 import argparse
 from tqdm import tqdm
+import difflib
 from title2bib.crossref import get_bib_from_title
+from contextlib import suppress
 
-def parse_arg():
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='input.txt')
     parser.add_argument('--output', type=str, default='output.txt')
     args = parser.parse_args()
-    return args
 
-def main():
-    args = parse_arg()
-    file = args.input
-    with open(file, 'r') as f:
-        titles = f.read()
-    titles = titles.split('\n')
+    with open(args.input, 'r', encoding='utf-8') as f:
+        titles = f.read().splitlines()
 
     failed_cnt = 0
     result = []
 
     for title in tqdm(titles):
-        try:
-            bib = get_bib_from_title(title, True)
-            result.append(bib[1])
-        except:
-            failed_cnt += 1
-            result.append("=== Failed === " + title + "\n")
+        if title.startswith("#") or not title.strip():
+            result.append(title + "\n")
+            continue
+        if title.startswith("@") or not title.strip():
+            result.append(title + "\n")
+            continue
 
-    with open(args.output, 'w+', encoding='utf-8') as f:
-        f.write(''.join(result))
-    
-    print("All: ", len(titles), " Failed: ", failed_cnt, " Successful: ", len(result))
-    
+        with suppress(Exception):
+            bib = get_bib_from_title(title, True)[1]
+            start = bib.find("title={")
+            end = bib.find("}", start)
+            title_from_bib = bib[start + len("title={"):end]
+            # print(f"Title from input: {title}")
+            # print(f"Title from bib  : {title_from_bib}")
+
+
+            similarity = difflib.SequenceMatcher(None, title.lower().strip(), title_from_bib.lower().strip()).ratio()
+            if similarity > 0.9:
+                result.append(bib.strip() + "\n")
+            else:
+                raise ValueError("Title mismatch")
+
+            continue  # skip fail count
+
+        failed_cnt += 1
+        result.append(f"=== Failed === {title}\n")
+
+    with open(args.output, 'w', encoding='utf-8') as f:
+        f.writelines(result)
+
+    total = len(titles)
+    print(f"All: {total}  Failed: {failed_cnt}  Successful: {total - failed_cnt}")
+
 if __name__ == '__main__':
     main()
